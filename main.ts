a9454c71f2b0cae45ac56bb49c8e281e2e112dc8
shanzay <meriqainat69@gmail.com>
2025-01-29 15:26:47 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,20 @@
+// Q2:
+// let personname ="shanza";
+// console.log('hello',personname, "would you like to learn some typescript today");
+//Q3: 
+//let personname ="shanza";
+//console.log(personname.toLowerCase());
+//console.log(personname.toUpperCase());
+//console.log(personname.charAt(0).toUpperCase()+personname.slice(1).toLowerCase());
+//Q4:
+//console.log("Allama iqbal one said",'"islam is itself destiny and will not suffer destiny"');
+//Q5:
+//let famousperson ="Allama iqbal";
+//let message ="one said,'islam is itself destiny and will not suffer destiny'";
+//console.log(famousperson,message);
+//Q6:
+let whitespace="\n\t shanza khan\t\n";
+console.log(whitespace);                                                                                                  
+
+let withoutwhitespace=whitespace.trim();
+console.log(withoutwhitespace); 
